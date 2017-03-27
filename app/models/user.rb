8f9cac30d626126c973dd58498ac16668ac38def
smithwebtek <brad@smithwebtek.com>
2017-03-27 19:33:22 -0400

    contact actions show tables working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
+          # :omniauthable, omniauth_providers: [:github]
+
   has_many :actions
   has_many :jobs, through: :actions
   has_many :contacts, through: :actions
   has_many :orgs, through: :actions
   has_many :steps, through: :actions
   has_many :resources
+
+  validates :email, presence: true, uniqueness: true
+
 end
