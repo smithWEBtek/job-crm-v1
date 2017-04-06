5f57395895e839a73cbcf18288bf72453055582b
smithwebtek <brad@smithwebtek.com>
2017-04-06 14:18:01 -0400

    add page-specific js to users.index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( tools.js )
 Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( users.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
